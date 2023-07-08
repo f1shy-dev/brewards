5351c91dba6d8445ef510c4a2b4116d6631d7740
f1shy-dev <56125930+f1shy-dev@users.noreply.github.com>
2023-07-08 21:30:13 +0100

    nice edge check

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const openWindow = (...args) =>
     if (!win || win.closed) return s();
     setTimeout(() => (win.innerHeight > 0 && !win.closed ? s(win) : s()), 200);
   });
-
+import { words } from "./words";
 const ck = async (emoji, text, msg = null) => {
   const item = document.createElement("div");
   item.className =
@@ -48,10 +48,14 @@ const ck = async (emoji, text, msg = null) => {
   return item;
 };
 
-const updateCk = async (item, emoji, text) => {
+const updateCk = async (item, emoji, text, msg = null) => {
   item.innerHTML = `<span>${
     emoji == "load" ? loader : emoji
-  }</span><span>${text}</span>`;
+  }</span><span class="${msg != null ? "flex flex-col" : ""}">${
+    msg == null
+      ? text
+      : ` <span>${text}</span><span class="text-gray-600 text-xs font-mono">${msg}</span>`
+  }</span>`;
 };
 
 (async () => {
@@ -66,11 +70,16 @@ const updateCk = async (item, emoji, text) => {
     check.close();
   }
   if (!hasOpened) {
-    updateCk(loader, "load", "Please allow pop-ups to continue...");
+    updateCk(
+      loader,
+      "load",
+      "Please allow pop-ups to continue...",
+      "😦 They're required to open Bing..."
+    );
     while (!hasOpened) {
-      await sleep(5000);
+      await sleep(2000);
       check = await openWindow("check.html", "_blank");
-      if (check.innerHeight) {
+      if (check && check.innerHeight) {
         hasOpened = true;
         check.close();
         await updateCk(loader, "✅", "Pop-ups allowed!");
@@ -81,7 +90,8 @@ const updateCk = async (item, emoji, text) => {
   check.close();
   await updateCk(loader, "✅", "Pop-ups allowed!");
 
-  if (navigator.userAgent.indexOf("Edg/") == -1) {
+  const edge = navigator.userAgent.indexOf("Edg/") != -1;
+  if (!edge) {
     await ck(
       "😬",
       "Edge browser not detected...",
@@ -96,46 +106,44 @@ const updateCk = async (item, emoji, text) => {
       navigator.userAgent
     ) && window.innerWidth < 768;
 
-  if (mobile) await ck("📱", "Mobile device detcted!");
-  else await ck("🖥", "Desktop device detected!");
+  const searches = (mobile ? 20 : 30) + (edge ? 4 : 0);
+  const skS = `Will do ${searches} searches for ${mobile ? 60 : 90}${
+    edge ? "+12" : ""
+  } points...`;
+  if (mobile) await ck("📱", "Mobile device detcted!", skS);
+  else await ck("🖥", "Desktop device detected!", skS);
 
-  let loading_words = await ck("load", "Downloading list of words...");
-  let words = await fetch(
-    "https://raw.githubusercontent.com/david47k/top-english-wordlists/master/top_english_nums_lower_500.txt"
-  );
-
-  if (!words.ok) {
-    await updateCk(loading_words, "❌", "Failed to download words!");
-    throw new Error("Failed to download words!");
-  }
-
-  words = await words.text();
-  words = words.split("\n");
-  words = words.map((w) => w.trim());
-  words = words.filter((w) => w.length > 2);
   shuffle(words);
-  await updateCk(loading_words, "📄", "Downloaded list of words!");
-  const count = mobile ? 24 : 34;
+  await ck("📄", `Loaded list of english words...`);
+
   let done = 0;
-  let tabs = await ck("3️⃣", `Starting ${count} searches...`);
+  let tabs = await ck("3️⃣", `Starting ${searches} searches...`);
   //   count down 3 seconds with emoji
   await sleep(1000);
-  await updateCk(tabs, "2️⃣", `Starting ${count} searches...`);
+  await updateCk(tabs, "2️⃣", `Starting ${searches} searches...`);
   await sleep(1000);
-  await updateCk(tabs, "1️⃣", `Starting ${count} searches...`);
+  await updateCk(tabs, "1️⃣", `Starting ${searches} searches...`);
   await sleep(1000);
-  await updateCk(tabs, "🚦", `Starting ${count} searches...`);
+  await updateCk(tabs, "🚦", `Starting ${searches} searches...`);
   await sleep(250);
-  await updateCk(tabs, "load", `Running searches (${done}/${count})...`);
+  const pageTitle = document.querySelector("title");
+  const updateTitle = (msg = null) => {
+    pageTitle.innerHTML =
+      msg != null ? msg : `(${done}/${searches}) - Bing Rewards Auto-Search`;
+  };
+  await updateCk(tabs, "load", `Running searches (${done}/${searches})...`);
+  updateTitle();
   await sleep(250);
-  while (done < count) {
+  while (done < searches) {
+    done++;
+    await updateCk(tabs, "load", `Running searches (${done}/${searches})...`);
+    updateTitle();
+
     const tab = window.open("https://bing.com/search?q=" + words[done]);
     await sleep(1200);
     tab.close();
-    done++;
-    await updateCk(tabs, "load", `Running searches (${done}/${count})...`);
     await sleep(700);
   }
 
-  await updateCk(tabs, "🎊", `Ran ${count} searches successfully!`);
+  await updateCk(tabs, "🎊", `Ran ${searches} searches successfully!`);
 })();
